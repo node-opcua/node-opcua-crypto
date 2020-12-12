b233ba4e4a371ba4e2117569a35feeaccabe5a0f
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-12-12 22:36:13 +0100

    detect electron

--- index_web.js
+++ index_web.js
@@ -1,1 +1,28 @@
-module.exports = require("./dist/source");
+function isElectron() {
+    // Renderer process
+    if (typeof window !== "undefined" && typeof window.process === "object" && window.process.type === "renderer") {
+        return true;
+    }
+
+    // Main process
+    if (typeof process !== "undefined" && typeof process.versions === "object" && !!process.versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === "object" && typeof navigator.userAgent === "string" && navigator.userAgent.indexOf("Electron") >= 0) {
+        return true;
+    }
+
+    return false;
+}
+if (isElectron()) {
+    module.exports = {
+        ...require("./dist/source"),
+        ...require("./dist/source_nodejs"),
+    };
+} else {
+    module.exports = {
+        ...require("./dist/source"),
+    };
+}
