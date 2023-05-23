6ee6046b993bd45b64741d381aee9a325d707d1a
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-05-23 13:43:20 +0200

    improve web example

--- web/esbuild.mjs
+++ web/esbuild.mjs
@@ -3,13 +3,17 @@ import { build } from "esbuild";
 import { polyfillNode } from "esbuild-plugin-polyfill-node";
 
 build({
-    entryPoints: ["web/main.js"],
+    entryPoints: ["web/main.ts"],
     bundle: true,
     outfile: "web/bundle.js",
     sourcemap: true,
+    // minify: true,
     plugins: [
         polyfillNode({
+            polyfills: {
+                crypto: true, // 'rollup-plugin-node-polyfills/polyfills/crypto-browserify'
             // Options (optional)
+            }
         }),
     ],
 });
