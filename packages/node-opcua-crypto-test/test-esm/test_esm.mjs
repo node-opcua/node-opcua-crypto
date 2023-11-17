8a78e4d7a21fae48fba2ef19c7a9c9123366115b
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-11-17 23:36:53 +0100

    update packages and esm export

---
+++ packages/node-opcua-crypto-test/test-esm/test_esm.mjs
@@ -0,0 +1,9 @@
+import { exploreCertificateInfo } from "../../node-opcua-crypto/index.mjs";
+// eslint-disable-next-line no-undef
+
+
+try {
+    exploreCertificateInfo(Buffer.from("123"));
+} catch (err) {
+    console.log(err.message);
+}
